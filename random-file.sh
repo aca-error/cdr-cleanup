6cc76b39d096705cd906886cfa2ac9d25efe69a6
aca-error <239507485+aca-error@users.noreply.github.com>
2026-01-28 10:08:26 +0700

    Update random-file.sh

--- random-file.sh
+++ random-file.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Create test files dengan berbagai umur dalam 3 direktori
+mkdir /home/cdrsbx/random1 /home/cdrsbx/random2 /home/cdrsbx/random3
 for i in {1..1000}; do
     touch -d "$((RANDOM % 365)) days ago" /home/cdrsbx/random1/file_$i.txt
     touch -d "$((RANDOM % 365)) days ago" /home/cdrsbx/random2/file_$i.txt
